56a2755b888fcb96b79ba0457b5e6081b87fd05f
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-07-23 20:13:20 -0400

    Postgresql connection

---
+++ postgreSQLdatabse/src/test/java/com/yuvam/postgreSQLdatabse/PostgreSqLdatabseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuvam.postgreSQLdatabse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqLdatabseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
